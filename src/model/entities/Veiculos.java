452698ddda0fd18f66ebabfc38649c03bcccaf5e
joaovinicius4 <joaoruas111@gmail.com>
2026-01-20 09:06:09 -0300

    Adiciona classe veículos

---
+++ src/model/entities/Veiculos.java
@@ -0,0 +1,18 @@
+package model.entities;
+
+public class Veiculos {
+	private String modelo;
+
+	public Veiculos(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+}
