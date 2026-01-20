c44b44c9afd09e3ee7c11cdaa3d6f0b7527b0299
joaovinicius4 <joaoruas111@gmail.com>
2026-01-20 10:50:05 -0300

    Adiciona construtores e modificadores na fatura

--- src/model/entities/AluguelCarro.java
+++ src/model/entities/AluguelCarro.java
@@ -7,5 +7,48 @@ public class AluguelCarro {
 	private LocalDateTime inicio;
 	private LocalDateTime fim;
 	private Veiculos veiculo;
+	private Fatura fatura;
+	
+	public AluguelCarro(LocalDateTime inicio, LocalDateTime fim, Veiculos veiculo, Fatura fatura) {
+		this.inicio = inicio;
+		this.fim = fim;
+		this.veiculo = veiculo;
+		this.fatura = fatura;
+	}
+
+	public LocalDateTime getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(LocalDateTime inicio) {
+		this.inicio = inicio;
+	}
+
+	public LocalDateTime getFim() {
+		return fim;
+	}
+
+	public void setFim(LocalDateTime fim) {
+		this.fim = fim;
+	}
+
+	public Veiculos getVeiculo() {
+		return veiculo;
+	}
+
+	public void setVeiculo(Veiculos veiculo) {
+		this.veiculo = veiculo;
+	}
+
+	public Fatura getFatura() {
+		return fatura;
+	}
+
+	public void setFatura(Fatura fatura) {
+		this.fatura = fatura;
+	}
+	
+	
+	
 	
 }
