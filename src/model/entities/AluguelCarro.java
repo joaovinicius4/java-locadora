0b747d56652084a06942328b71a07d4f54011dd5
joaovinicius4 <joaoruas111@gmail.com>
2026-01-20 09:31:57 -0300

    Adiciona classe AluguelCarro

---
+++ src/model/entities/AluguelCarro.java
@@ -0,0 +1,11 @@
+package model.entities;
+
+import java.time.LocalDateTime;
+
+public class AluguelCarro {
+	
+	private LocalDateTime inicio;
+	private LocalDateTime fim;
+	private Veiculos veiculo;
+	
+}
